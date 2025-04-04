34d5d5dacb2e57bf4f9b5d659560a7246ca40c4b
TGubbels <t.j.g.m.gubbels@gmail.com>
2025-04-04 12:33:17 +0200

    Start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { BreakpointObserver } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatIconModule,MatButtonModule, MatToolbarModule, MatSidenavModule, MatListModule, MatSidenav],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'ITalentPortfolio';
+  title = 'material-responsive-sidenav';
+  @ViewChild(MatSidenav)
+  sidenav!: MatSidenav;
+  isMobile= true;
+  isCollapsed = true;
+
+
+  constructor(private observer: BreakpointObserver) {}
+
+
+  toggleMenu() {
+    this.isCollapsed = !this.isCollapsed;
+  }
 }
